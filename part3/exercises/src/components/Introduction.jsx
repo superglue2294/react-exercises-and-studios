826a3f8368fb1730f2f328851da10334f4b2b47d
Tri <superglue2294@gmail.com>
2024-08-26 16:05:22 -0400

    react exercises part 3 complete

--- part3/exercises/src/components/Introduction.jsx
+++ part3/exercises/src/components/Introduction.jsx
@@ -1,11 +1,11 @@
 export default function HobbyIntroduction () {
    return(
       <div>
-         <h2>3 Fun Facts About Needlepoint</h2>
+         <h2>3 Fun Facts About Photography</h2>
          <ol>
-            <li>Needlepoint uses a specialized canvas as it's base.</li>
-            <li>The most basic stitch is called a tent stitch.</li>
-            <li>Needlepoint uses a lot of different kinds of fibers.</li>
+            <li>The word “Photography” comes from the Greek, meaning to draw with light. The earliest known use of the word photograph as we know it was in 1839 by the astronomer Sir John Herschel. <a href="https://www.photoion.co.uk/blog/interesting-photography-facts/">Source</a></li>
+            <li>The first photograph ever taken was by a French scientist named Joseph Nicéphore Niépce. He captured the image with a camera obscura in 1826. The famous image took eight hours to capture because of the long exposure needed for the camera. The image was actually lost for nearly 50 years before historians rediscovered it. <a href="https://www.photoion.co.uk/blog/interesting-photography-facts/">Source</a></li>
+            <li>Some of the most popular subjects of photography in the 1800s were corpses. Many famous individuals were famously photographed after their deaths, like the Clantons and McLaurys who died in the Gunfight at the O.K. Corral. But it wasn't only the famous who found themselves as posthumous models; regular people were often photographed after death also. <a href="https://www.photoion.co.uk/blog/interesting-photography-facts/">Source</a></li>
          </ol>
       </div>
    );
--- part3/exercises/src/components/Projects.jsx
+++ part3/exercises/src/components/Projects.jsx
@@ -2,17 +2,15 @@ import data from '../data.json';
 import { useState } from 'react';
 
 export default function MyProjects() {
-   const [index, setIndex] = useState(0);
+  const [index, setIndex] = useState(0);
 
   function handleClick() {
-   if (index < data.projects.length-1)
-   {
+    if (index < data.projects.length - 1) {
       setIndex(index + 1);
-   }
-   else
-   {
+    }
+    else {
       setIndex(0);
-   }
+    }
   }
 
   let projects = data.projects;
@@ -23,7 +21,7 @@ export default function MyProjects() {
       <button onClick={handleClick}>
         Next
       </button>
-      <h2>{project.canvas} by {project.designer}
+      <h2>{project.image} by {project.photographer}
       </h2>
       <img src={project.photoUrl} alt={project.alt} />
     </div>
